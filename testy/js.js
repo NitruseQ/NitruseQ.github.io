2f541fa6f4deca3bec5c45fc8eca4d0cff55f8cb
NitruseQ <rafal.sz@tlen.pl>
2020-10-22 01:31:35 +0200

    zmiana wielkosci

---
+++ testy/js.js
@@ -0,0 +1,33 @@
+function rozmiar(value) {
+document.getElementById('twitch-embed').style.width = value + "%";
+document.getElementById('twitch-embed').style.height = value + "%";
+}
+
+function czat(value) {
+document.getElementById('chat_embed').style.width = value + "px";
+szerokosc = window.innerWidth;
+szerokosc2 = szerokosc - value;
+document.getElementById('filmframe').style.width = szerokosc2 + "px";
+}
+
+function klik() {
+  var checkBox = document.getElementById("switch");
+  if (checkBox.checked == true){
+    document.getElementById('twitch-embed').style.display = "block";
+  } else {
+    document.getElementById('twitch-embed').style.display = "none";
+  }
+}
+
+function klik2() {
+  var checkBox2 = document.getElementById("switch2");
+  if (checkBox2.checked == true){
+    document.getElementById('suwak').style.display = "block";
+	document.getElementById('suwak2').style.display = "block";
+	document.getElementById('switch').style.display = "block";
+  } else {
+    document.getElementById('suwak').style.display = "none";
+	document.getElementById('suwak2').style.display = "none";
+	document.getElementById('switch').style.display = "none";
+  }
+}
