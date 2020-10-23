068a09e9eb1adda48a1d89832332766011e2e521
NitruseQ <rafal.sz@tlen.pl>
2020-10-24 01:42:30 +0200

    losowa strona glowna

---
+++ losowanie.js
@@ -0,0 +1,16 @@
+liczba = Math.floor(Math.random() * 4);
+if (liczba == 0) {
+	document.getElementById('tlo1').src = "1.gif";
+}
+
+if (liczba == 1) {
+	document.getElementById('tlo1').src = "2.png";
+}
+
+if (liczba == 2) {
+	document.getElementById('tlo1').src = "3.png";
+}
+
+if (liczba == 3) {
+	document.getElementById('tlo1').src = "4.gif";
+}
