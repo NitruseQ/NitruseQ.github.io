7896e54f60b3e3f87f8984f7f6cc2d85c4694252
NitruseQ <rafal.sz@tlen.pl>
2020-10-24 13:52:05 +0200

    rth

--- losowanie.js
+++ losowanie.js
@@ -1,24 +1,24 @@
 liczba = Math.floor(Math.random() * 6);
 if (liczba == 0) {
-	document.getElementById('tlo1').src = "1.gif";
+	document.getElementById('tlo1').src = "png/1.gif";
 }
 
 if (liczba == 1) {
-	document.getElementById('tlo1').src = "2.png";
+	document.getElementById('tlo1').src = "png/2.png";
 }
 
 if (liczba == 2) {
-	document.getElementById('tlo1').src = "3.png";
+	document.getElementById('tlo1').src = "png/3.png";
 }
 
 if (liczba == 3) {
-	document.getElementById('tlo1').src = "4.gif";
+	document.getElementById('tlo1').src = "png/4.gif";
 }
 
 if (liczba == 4) {
-	document.getElementById('tlo1').src = "5.png";
+	document.getElementById('tlo1').src = "png/5.png";
 }
 
 if (liczba == 5) {
-	document.getElementById('tlo1').src = "6.png";
+	document.getElementById('tlo1').src = "png/6.png";
 }
