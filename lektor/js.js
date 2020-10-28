1fe56e800acfb98d9eb71f0fcf5c48f8135cd816
NitruseQ <rafal.sz@tlen.pl>
2020-10-28 16:32:25 +0100

    mozliwosc wylaczenia znikania paska

--- lektor/js.js
+++ lektor/js.js
@@ -13,6 +13,8 @@ szerokosc5 = szerokosc2 - 149;
 szerokosc6 = szerokosc2 - 128;
 szerokosc7 = szerokosc2 - 226;
 szerokosc8 = szerokosc2 - 323;
+szerokosc9 = szerokosc2 - 344;
+szerokosc10 = szerokosc2 - 435;
 document.getElementById('filmframe').style.width = szerokosc2 + "px";
 document.getElementById('text').style.left = szerokosc3 + "px";
 document.getElementById('switch').style.left = szerokosc5 + "px";
@@ -21,6 +23,8 @@ document.getElementById('opcje').style.left = szerokosc6 + "px";
 document.getElementById('ukryjstream').style.left = szerokosc7 + "px";
 document.getElementById('zmiana').style.left = szerokosc8 + "px";
 document.getElementById('zaslaniacz').style.width = szerokosc2 + "px";
+document.getElementById('switch3').style.left = szerokosc9 + "px";
+document.getElementById('pasek').style.left = szerokosc10 + "px";
 }
 
 function klik() {
@@ -43,6 +47,10 @@ function klik2() {
 	document.getElementById('ukryjstream').style.display = "block";
 	document.getElementById('opcje').style.color = "#ffffff";
 	document.getElementById('zmiana').style.display = "block";
+	if (czy_yt == 0) {
+		document.getElementById('switch3').style.display = "block";
+		document.getElementById('pasek').style.display = "block";
+	}
   } else {
     document.getElementById('suwak').style.display = "none";
 	document.getElementById('suwak2').style.display = "none";
@@ -52,8 +60,19 @@ function klik2() {
 	document.getElementById('ukryjstream').style.display = "none";
 	document.getElementById('opcje').style.color = "#808080";
 	document.getElementById('zmiana').style.display = "none";
+	document.getElementById('switch3').style.display = "none";
+	document.getElementById('pasek').style.display = "none";
   }
 }
+
+function klik3() {
+	var checkBox = document.getElementById("switch3");
+	if ((checkBox.checked == true) && (czy_yt == 0)){
+		document.getElementById('zaslaniacz').style.display = "block";
+	} else {
+		document.getElementById('zaslaniacz').style.display = "none";
+	}
+  }
 
 function zmiana() {
 	var zrodlo = prompt('wybierz źródło filmu, wpisz:\n"cda", bądź "youtube"\n*zmiana nastąpi tylko u Ciebie, u nikogo innego');
@@ -70,6 +89,14 @@ function zmiana() {
 			else {
 			if (akceptacja == true) {
 				document.getElementById('filmframe').src = 'https://ebd.cda.pl/620x368/' + id;
+				czy_yt = 0;
+				if (document.getElementById('switch3').checked == true) {
+					document.getElementById('zaslaniacz').style.display = 'block';
+				}
+				if (document.getElementById('switch2').checked == true) {
+					document.getElementById('switch3').style.display = 'block';
+					document.getElementById('pasek').style.display = 'block';
+				}
 			}
 			else {
 				return 0;
@@ -90,6 +117,10 @@ function zmiana() {
 			else {
 			if (akceptacja == true) {
 				document.getElementById('filmframe').src = 'https://www.youtube.com/embed/' + id;
+				czy_yt = 1;
+				document.getElementById('zaslaniacz').style.display = 'none';
+				document.getElementById('switch3').style.display = 'none';
+				document.getElementById('pasek').style.display = 'none';
 			}
 			else {
 				return 0;
